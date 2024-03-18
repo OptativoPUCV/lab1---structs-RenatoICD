138a5500c0aaf7513f55a271d1fed427cf8f1e2c
Your Name <you@example.com>
2024-03-18 15:36:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -154,7 +154,7 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
   strcpy(libro->titulo, titulo);
   strcpy(libro->autor.nombre, nombreAutor);
   libro->autor.anioNacimiento = anioNacimiento;
-  // libro->anioPublicacion = anioPublicacion;
+  libro->anioPublicacion = anioPublicacion;
 }
 /*
 Ejercicio 7: Lista enlazada de números
